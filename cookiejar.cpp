eb5887f0a2317686ab990b0bbb7cdd4178b7cd7c
Markus Lindelöw <markus@lindeloew.se>
2012-08-06 23:56:28 +0200

    kakor

--- cookiejar.cpp
+++ cookiejar.cpp
@@ -1,33 +1,32 @@
 #include "cookiejar.h"
 
 CookieJar::CookieJar(QObject *parent) :
-    QNetworkCookieJar(parent)
+  QNetworkCookieJar(parent)
 {
-    jarfile = new QFile("/tmp/test", this);
-    if (jarfile->open(QIODevice::ReadWrite | QIODevice::Append | QIODevice::Text))
-    {
-        //Lägg till en massa cookies
-        //QNetworkCookie nc()
-    }
-
+  jarfile = new QFile("/tmp/kaktuskakor", this);
+  if (jarfile->open(QIODevice::ReadWrite | QIODevice::Text))
+  {
+    //Lägg till en massa cookies
+    QTextStream jf(jarfile);
+    setAllCookies(QNetworkCookie::parseCookies(jf.readAll().toAscii()));
+  }
 }
 
 bool CookieJar::setCookiesFromUrl(const QList<QNetworkCookie> &cookieList, const QUrl &url)
 {
-    if (jarfile->isOpen())
+  if (jarfile->isOpen())
+  {
+    QTextStream jf(jarfile);
+
+    foreach(QNetworkCookie nc, cookieList)
     {
-        QTextStream jf(jarfile);
-
-        jf << "----\n";
-        foreach(QNetworkCookie nc, cookieList)
-        {
-            if (!nc.isSessionCookie())
-            {
-                //QString data(nc.toRawForm());
-                //jf << data << "\n";
-            }
-        }
+      if (!nc.isSessionCookie())
+      {
+        QString data(nc.toRawForm());
+        jf << data << "\n";
+      }
     }
+  }
 
-    return QNetworkCookieJar::setCookiesFromUrl(cookieList, url);
+  return QNetworkCookieJar::setCookiesFromUrl(cookieList, url);
 }
